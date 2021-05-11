c780665df99f6f39355d6b7b843e55f17c0d45fa
Amid Esfahani <amid.esfahani@gmail.com>
2021-05-11 17:10:25 +0430

    fix field.js

--- resources/js/field.js
+++ resources/js/field.js
@@ -1,5 +1,5 @@
 Nova.booting((Vue, router, store) => {
-  Vue.component('index-color-picker', require('./components/IndexField'))
-  Vue.component('detail-color-picker', require('./components/DetailField'))
-  Vue.component('form-color-picker', require('./components/FormField'))
+  Vue.component('index-nova-color-picker', require('./components/IndexField'))
+  Vue.component('detail-nova-color-picker', require('./components/DetailField'))
+  Vue.component('form-nova-color-picker', require('./components/FormField'))
 })
